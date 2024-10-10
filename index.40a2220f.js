11fe840ef0fbddc1411aea5e831e2b4b58219bd2
SerhiiSemennikov <semennik@hotmail.com>
2024-10-10 16:42:09 +0200

    make build

---
+++ index.40a2220f.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("input").forEach(function(e){var l=document.createElement("label");l.className="field-label",l.htmlFor=e.id,l.textContent=e.name,e.placeholder=e.name.charAt(0).toUpperCase()+e.name.slice(1).replace("N"," N"),e.parentElement.appendChild(l)});
+//# sourceMappingURL=index.40a2220f.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const inputs = document.querySelectorAll("input");
+inputs.forEach((elementOfInput)=>{
+    const label = document.createElement("label");
+    label.className = "field-label";
+    label.htmlFor = elementOfInput.id;
+    label.textContent = elementOfInput.name;
+    elementOfInput.placeholder = elementOfInput.name.charAt(0).toUpperCase() + elementOfInput.name.slice(1).replace("N", " N");
+    elementOfInput.parentElement.appendChild(label);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
